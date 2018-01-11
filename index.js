cfc9b3430b2558b0361bb8b51cc8e5958c590f87
fornesarturo <fornesarturo@gmail.com>
2018-01-10 21:47:55 -0600

    compressed request

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 $('document').ready(function () {
     $.get("/title", function (data, status) {
         console.log("Data: " + data);
-        $("#title").html(function(i, text) {
-            return data
-        })
+        $("#title").html(data);
     });
 });
